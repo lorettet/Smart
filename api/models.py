9304237113000c1961d2328a1898bd9438d88699
Théo Lorette-Froidevaux <theo@rtone.fr>
2019-05-02 09:50:32 +0200

    add user model + login & create services

--- api/models.py
+++ api/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
 
 class User(models.Model):
-    firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
-    id = models.AutoField(primary_key=True)
+    @classmethod
+    def create(cls, firstname,lastname,email,password):
+        user = cls(firstname=firstname, lastname=lastname, email=email, password=password)
+        return user
+
+    def __str__(self):
+        return self.firstname+' '+self.lastname+' ('+self.email+')'
+
+    firstname = models.CharField(max_length=30,null=False)
+    lastname = models.CharField(max_length=30,null=False)
+    email = models.CharField(max_length=50,null=False,unique=True)
+    password = models.CharField(max_length=50,null=False)
