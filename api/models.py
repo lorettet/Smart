59674d86915911d0d1ea900d74997e47dc6bf01c
PAGILLES <pagilles@hotmail.com>
2019-05-02 11:31:38 +0200

    bd incroyable

--- api/models.py
+++ api/models.py
@@ -4,15 +4,41 @@ from django import forms
 # Create your models here.
 
 class User(models.Model):
+    email = models.CharField(max_length=50,null=False,unique=True)
+    password = models.CharField(max_length=50,null=False)
+    class Meta:
+        abstract = True
+
+class Client(User):
     @classmethod
-    def create(cls, firstname,lastname,email,password):
-        user = cls(firstname=firstname, lastname=lastname, email=email, password=password)
-        return user
+    def create(cls, firstname, lastname, email, password):
+        client = cls(firstname=firstname, lastname=lastname, email=email, password=password)
+        return client
 
     def __str__(self):
         return self.firstname+' '+self.lastname+' ('+self.email+')'
 
     firstname = models.CharField(max_length=30,null=False)
-    lastname = models.CharField(max_length=30,null=False)
-    email = models.CharField(max_length=50,null=False,unique=True)
-    password = models.CharField(max_length=50,null=False)
+    lastname = models.CharField(max_length=30,null=False)        
+
+class Store(User):
+    @classmethod
+    def create(cls, name, email, password):
+        store = cls(name=name, email=email, password=password)
+        return store
+
+    def __str__(self):
+        return self.name + '('+self.email+')'
+    
+    name = models.CharField(max_length=30,null=False)
+    
+'''
+class Product(models.Model):
+    category
+    name
+    store = 
+
+class ProductModel(models.Model)
+    category
+    name
+'''
--- api/views.py
+++ api/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello, wolrd!')
+    return HttpResponse('Hello, world! This is the Optifood api index.')
 
 def login(request):
     pass
+
+
+
+def get_stores_all(request):
+    stores = User.objects.filter(type="store")
+    stores_list = list(stores)  # important: convert the QuerySet to a list object
+    return JsonResponse(users_list, safe=False)
+
+# def get_stores(request):
